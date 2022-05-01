9923f323c9521874ae54191e4b8f6fcea616c21e
Shaun Wild <shaunwild97@gmail.com>
2022-05-02 00:24:05 +0100

    Getting release ready

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    `maven-publish`
 }
 
-group = "com.github.shaun-wild"
-version = "0.0.1"
+val groupId = "com.github.shaun-wild"
+val packageVersion = "1.0.0"
+
+group = groupId
+version = packageVersion
 
 repositories {
     mavenCentral()
@@ -14,6 +18,27 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupId
+            artifactId = "kproxy"
+            version = packageVersion
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
